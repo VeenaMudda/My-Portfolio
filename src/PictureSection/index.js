b9e7ef10f8ec6743bf0b23773253136f63af7e0a
Veena Mudda <apple@apples-MacBook-Pro.local>
2025-05-21 18:09:29 +0530

    Home page screen sizes

--- src/PictureSection/index.js
+++ src/PictureSection/index.js
@@ -11,11 +11,16 @@ const PictureSection = () => {
     return(
         <div className='picture-section-pictures-container'>
             {isSmallScreen ? 
-                <Carousel autoPlay={true} infiniteLoop={true} showStatus={false} showArrows={false} showIndicators={false} showThumbs={false} className='carousel'>
+                <Carousel autoPlay={true} infiniteLoop={true} dynamicHeight={true} showStatus={false} showArrows={false} showIndicators={false} showThumbs={false} className='carousel'>
                     <img src={makeup_artist} alt="makeup artist" className='picture'/>
                     <img src={self_makeup} alt="self makeup" className='picture'/>
                     <img src={look} alt='makeup look' className='picture'/>
-                </Carousel> : <div>b</div>}
+                </Carousel> : 
+                <div className='pictures-section-pictures-container'>
+                    <img src={makeup_artist} alt='makeup artist' className='picture'/>
+                    <img src={self_makeup} alt='self makeup' className='picture'/>
+                    <img src={look} alt='makeup look' className='picture'/>
+                </div>}
         </div>
     )
 }
